920a048d234c3897f37138cb91cea582b137458e
User <me@example.com>
2026-03-02 15:43:33 +0300

    second initial

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ async def index():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=3589, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=9011, reload=True)
