d8d60ff1b63ec680f79619d178d5ff4eaccf71ca
User <me@example.com>
2026-03-03 12:00:10 +0300

    Repository added

--- routers/task.py
+++ routers/task.py
@@ -1,43 +1,27 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter
 from typing import Annotated
 
-from sqlalchemy import select
-
 from database import SessionDep
-from models.tasks import TasksModel
 from schemas.task import STask, STaskAdd
+from repository import TaskRepository  # Импортируем наш новый класс
 
 router = APIRouter(prefix="/tasks", tags=["Задачи"])
 
 @router.post("", response_model=STask)
 async def create_task(
     task: STaskAdd,
-    session: SessionDep
+    session: SessionDep,
 ):
-    # 1. Превращаем Pydantic-схему в модель БД
-    # task.model_dump() вернет словарь {"name": "...", ...}
-    # ** - это распаковка словаря
-    new_task = TasksModel(**task.model_dump())
-    
-    # 2. Добавляем в сессию
-    session.add(new_task)
-    
-    # 3. Коммитим (сохраняем на диск)
-    await session.commit()
-    
-    # 4. Обновляем объект (получаем выданный ID)
-    await session.refresh(new_task)
-    
-    # 5. Возвращаем объект БД. Pydantic сам превратит его в JSON.
-    return new_task
+    # Вся логика сохранения ушла в репозиторий.
+    # Роутер просто передает данные и ждет результат.
+    task_model = await TaskRepository.add_one(task, session)
+    return task_model
 
-@router.get("")
-async def get_tasks(session: SessionDep):
-    # 1. Формируем запрос
-    query = select(TasksModel)
-    
-    # 2. Выполняем запрос
-    result = await session.execute(query)
-    
-    # 3. Получаем чистые объекты (scalars) и превращаем в список (all)
-    return result.scalars().all()
+@router.get("", response_model=list[STask])
+async def get_tasks(
+    session: SessionDep,
+):
+    # Роутер не знает, как выполняется поиск (SQL? Файл? API?).
+    # Ему нужен просто список задач.
+    tasks = await TaskRepository.find_all(session)
+    return tasks
